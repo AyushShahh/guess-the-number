4ae8c97267340dca3c7070a40f62f3c3da332f97
Ayush Shah <shahayush001@gmail.com>
2021-08-09 16:05:45 +0530

    feat: improved response of your guess

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ name = input('Hey there! What is your name?\n')
 
 number = random.randint(1, 100)
 
-print('\nWell %s... I am thinking of a number between 1 and 100\n' % name)
+print('\nWell %s... I am thinking of a number between 1 and 100.\n' % name)
 print('I want you to guess the number.')
-print('You have 8 guesses.\n')
+print('You have 8 guesses\n')
 
 while guesses_made < 9:
     guess = int(input('Make a guess: '))
     guesses_made += 1
 
     if guess < 1 or guess > 100:
-        print('> The number should be between 1 and 100.\n')
+        print('> The number is between 1 and 100.\n')
     if number > guess > 0:
-        print('> Your guess is low.\n')
+        print('> The number is higher than %d.\n' % guess)
     if 101 > guess > number:
-        print('> Your guess is high.\n')
+        print('> The number is lower than %d.\n' % guess)
     if guess == number:
         break
 
 if guess == number:
-    print('Great job %s! You guessed my number in %d guesses.' % (name, guesses_made))
+    print('\nGreat job %s! You guessed the number in %d guesses.' % (name, guesses_made))
 else:
-    print('You ran out of your guesses. The number I was thinking was %d.' % number)
+    print('\nYou ran out of your guesses. The number I was thinking was %d.' % number)
